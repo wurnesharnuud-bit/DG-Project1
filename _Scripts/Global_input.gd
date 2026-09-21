bb8edcaba3c5da9f479ff103c89d1d60b2908552
The West <wurnesharnuud@gmail.com>
2026-09-21 13:05:12 +0200

    esc TO ESC

---
+++ _Scripts/Global_input.gd
@@ -0,0 +1,14 @@
+extends Node
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"):
+		get_tree().quit()
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
